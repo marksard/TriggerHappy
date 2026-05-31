4e7a258d9862e03d9a602b68323e0b424ecd3fc0
marksard <marksard@rocketmail.com>
2026-05-31 09:25:06 +0900

    Refactor akimbo's commonlib code. Change the CPU speed of the Trigger Happy app.

--- app/common/triggerhappy_basicconfig.h
+++ app/common/triggerhappy_basicconfig.h
@@ -9,14 +9,13 @@
 #include <Arduino.h>
 
 // 基本使用設定
-#define CPU_CLOCK 133000000.0
 #define INTR_PWM_RESO 512
 #define PWM_BIT 11
 #define PWM_RESO 2048
 #define ADC_BIT 12
 #define ADC_RESO 4096
-#define DAC_MAX_MILLVOLT 5000 // mV
-#define SAMPLE_FREQ ((CPU_CLOCK / INTR_PWM_RESO) / 8) // 32470.703125khz
+// #define DAC_MAX_MILLVOLT 5000 // mV
+#define SAMPLE_FREQ (((float)(CPU_CLOCK) / (float)INTR_PWM_RESO) / (float)SAMPLE_FREQ_DIVIDER)
 
 // 出力数
 #define OUT_COUNT 6
