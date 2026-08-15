fe22985d76ad9e7b1e40cb355c74bcde1f8000b7
marksard <marksard@rocketmail.com>
2026-08-15 23:39:17 +0900

    Add Clock Divider

---
+++ app/ClockDivider/src/main.cpp
@@ -0,0 +1,361 @@
+/*!
+ * ClockDivider
+ * Copyright 2026 marksard
+ * This software is released under the MIT license.
+ * see https://opensource.org/licenses/MIT
+ */
+
+#include <Arduino.h>
+#include <numeric>
+#include <hardware/pwm.h>
+#include <hardware/irq.h>
+#include <hardware/gpio.h>
+#include <EEPROM.h>
+#include "lib/Button.hpp"
+#include "lib/RotaryEncoder.hpp"
+#include "lib/RGBLEDPWMControl.hpp"
+#include "lib/EEPROMConfigIO.hpp"
+#include "triggerhappy_gpio.h"
+#include "triggerhappy_basicconfig.h"
+#include "SystemConfig.hpp"
+
+#include "lib/pwm_wrapper.h"
+#include "lib/EdgeChecker.hpp"
+#include "lib/TriggerOut.hpp"
+#include "TriggerOutManager.hpp"
+
+#include "ClockDividerMultiplier.hpp"
+#include "SyncLFO.hpp"
+
+enum ButtonCondition
+{
+    // 各ボタンの押下状態と各ボタンの組み合わせ
+    // Button State: 0:None 1:Button down 2:Button up 3:Holding 4:Holded
+    // U: button up
+    // D: button down
+    // H: holging
+    // L: holded (leaved)
+    // 0xMABR (Mode, A, B, RE(RotaryEncoder) button)
+    NONE = 0x0000,
+    UA = 0x0200,
+    UB = 0x0020,
+    UMODE = 0x2000,
+    URE = 0x0002,
+    HA = 0x0300,
+    HB = 0x0030,
+    HM = 0x3000,
+    HA_HB = 0x0330,
+    HM_URE = 0x3002,
+};
+
+// 標準インターフェース
+// static uint interruptSliceNum;
+static RotaryEncoder enc;
+static Button buttons[4];
+static RGBLEDPWMControl rgbLedControl;
+static RGBLEDPWMControl::MenuColor menuColor = RGBLEDPWMControl::MenuColor::GREEN;
+static EEPROMConfigIO<SystemConfig> systemConfig(0);
+
+// 機能
+static int8_t trigModeIndex = 0;
+// static EdgeChecker clockEdge;
+// static EdgeChecker resetEdge;
+static EdgeChecker dataEdge;
+static volatile bool clockEdgeLatch = false;
+static volatile bool resetEdgeLatch = false;
+static volatile bool dataEdgeLatch = false;
+static TriggerOutManager triggerOutManager;
+
+class ClockDiviImplA : public ClockDividerMultiplier
+{
+public:
+    ClockDiviImplA(uint8_t numChannels): ClockDividerMultiplier(numChannels) {}
+
+private:
+    void onOutputHigh(uint8_t ch) override
+    {
+        triggerOutManager.out(ch)->set(1);
+    }
+
+    void onOutputLow(uint8_t ch) override
+    {
+        triggerOutManager.out(ch)->set(0);
+    }
+};
+ClockDiviImplA clockDivA(3);
+class ClockDiviImplB : public ClockDividerMultiplier
+{
+public:
+    ClockDiviImplB(uint8_t numChannels): ClockDividerMultiplier(numChannels) {}
+
+private:
+    void onOutputHigh(uint8_t ch) override
+    {
+        triggerOutManager.out(ch + 3)->set(1);
+    }
+
+    void onOutputLow(uint8_t ch) override
+    {
+        triggerOutManager.out(ch + 3)->set(0);
+    }
+};
+ClockDiviImplB clockDivB(3);
+
+SyncLFO lfo;
+
+//////////////////////////////////////////
+
+template <typename vs = int8_t>
+vs constrainCyclic(vs value, vs min, vs max)
+{
+    if (value > max)
+        return min;
+    if (value < min)
+        return max;
+    return value;
+}
+
+//////////////////////////////////////////
+
+void addTriggerModeCh(int8_t delta)
+{
+    trigModeIndex = constrainCyclic(trigModeIndex + delta, 0, 6);
+    const RGBLEDPWMControl::MenuColor menuColors[OUT_COUNT + 1] = {
+        RGBLEDPWMControl::MenuColor::GREEN,
+        RGBLEDPWMControl::MenuColor::GREEN,
+        RGBLEDPWMControl::MenuColor::YELLOW,
+        RGBLEDPWMControl::MenuColor::YELLOW,
+        RGBLEDPWMControl::MenuColor::RED,
+        RGBLEDPWMControl::MenuColor::RED,
+        RGBLEDPWMControl::MenuColor::CYAN};
+    const int8_t menuLevels[OUT_COUNT + 1] = {
+        5, 11,
+        5, 11,
+        5, 11,
+        11};
+
+    rgbLedControl.setMenuColor(menuColors[trigModeIndex]);
+    rgbLedControl.setMenuColorLevel(menuLevels[trigModeIndex]);
+}
+
+void operation(uint16_t buttonStates, int8_t encValue)
+{
+    if (buttonStates == ButtonCondition::UA)
+    {
+        addTriggerModeCh(-1);
+    }
+    else if (buttonStates == ButtonCondition::UB)
+    {
+        addTriggerModeCh(1);
+    }
+    if (buttonStates == ButtonCondition::URE)
+    {
+        clockDivA.onResetRise(true);
+        clockDivB.onResetRise(true);
+        lfo.onResetRise(true);
+        clockEdgeLatch = true;
+    }
+    if (buttonStates == ButtonCondition::HA)
+    {
+        lfo.addWaveform(encValue);
+    }
+    if (buttonStates == ButtonCondition::HB)
+    {
+    }
+    else if (buttonStates == ButtonCondition::UMODE)
+    {
+        if (trigModeIndex < 3)
+        {
+            clockDivA.channels[trigModeIndex].togglePulseMode();
+        }
+        else
+        {
+            clockDivB.channels[trigModeIndex - 3].togglePulseMode();
+        }
+    }
+    else if (buttonStates == ButtonCondition::NONE)
+    {
+        if (trigModeIndex == 6)
+        {
+            lfo.addRatio(encValue);
+        }
+        else if (trigModeIndex < 3)
+        {
+            clockDivA.channels[trigModeIndex].addRatio(encValue);
+        }
+        else
+        {
+            clockDivB.channels[trigModeIndex - 3].addRatio(encValue);
+        }
+    }
+}
+
+void process()
+{
+    if (resetEdgeLatch)
+    {
+        resetEdgeLatch = false;
+        clockDivA.onResetRise();
+        clockDivB.onResetRise();
+        lfo.onResetRise();
+        clockEdgeLatch = true;
+    }
+    else
+    {
+        if (clockEdgeLatch)
+        {
+            clockEdgeLatch = false;
+            lfo.onClockRise();
+            clockDivA.onClockRise();
+            if (!dataEdge.isAlive())
+            {
+                clockDivB.onClockRise();
+            }
+        }
+
+        if (dataEdgeLatch)
+        {
+            dataEdgeLatch = false;
+            clockDivB.onClockRise();
+        }
+    }
+
+    clockDivA.update();
+    clockDivB.update();
+    lfo.update();
+
+    pwm_set_gpio_level(OUT_CV, lfo.getValue());
+    triggerOutManager.process();
+}
+
+//////////////////////////////////////////
+
+void edgeCallback(uint gpio, uint32_t events)
+{
+    if (gpio == CLOCK)
+    {
+        if (events & GPIO_IRQ_EDGE_RISE)
+        {
+            // clockEdge.updateEdge(1);
+            clockEdgeLatch = true;
+        }
+        else if (events & GPIO_IRQ_EDGE_FALL)
+        {
+            // clockEdge.updateEdge(0);
+            clockEdgeLatch = false;
+        }
+    }
+    else if (gpio == RESET)
+    {
+        if (events & GPIO_IRQ_EDGE_RISE)
+        {
+            // resetEdge.updateEdge(1);
+            resetEdgeLatch = true;
+        }
+        else if (events & GPIO_IRQ_EDGE_FALL)
+        {
+            // resetEdge.updateEdge(0);
+            resetEdgeLatch = false;
+        }
+    }
+    else if (gpio == DATA)
+    {
+        if (events & GPIO_IRQ_EDGE_RISE)
+        {
+            dataEdge.updateEdge(1);
+            dataEdgeLatch = true;
+        }
+        else if (events & GPIO_IRQ_EDGE_FALL)
+        {
+            dataEdge.updateEdge(0);
+            dataEdgeLatch = false;
+        }
+    }
+}
+
+// void interruptPWM()
+// {
+//     pwm_clear_irq(interruptSliceNum);
+// }
+
+void setup()
+{
+    set_sys_clock_hz(CPU_CLOCK, true);
+    pinMode(23, OUTPUT);
+    gpio_put(23, HIGH);
+
+    enc.init(EC1B, EC1A, true);
+    buttons[0].init(BTN_A);
+    buttons[0].setHoldTime(350);
+    buttons[1].init(BTN_B);
+    buttons[1].setHoldTime(350);
+    buttons[2].init(BTN_RE, false, false, true);
+    buttons[2].setHoldTime(500);
+    buttons[3].init(BTN_MODE);
+    buttons[3].setHoldTime(350);
+    // clockEdge.init(CLOCK);
+    // resetEdge.init(RESET);
+    dataEdge.init(DATA, 2000); // 入力チェックのみ利用
+    triggerOutManager.init();
+
+    rgbLedControl.init(20000, PWM_BIT, LED_R, LED_G, LED_B);
+    rgbLedControl.setMenuColor(menuColor);
+
+    systemConfig.initEEPROM();
+    systemConfig.loadUserConfig();
+
+    clockDivA.init();
+    clockDivA.channels[0].setRatio(ClockDividerMultiplier::RatioIndex::CLK);
+    clockDivA.channels[1].setRatio(ClockDividerMultiplier::RatioIndex::DIV2);
+    clockDivA.channels[2].setRatio(ClockDividerMultiplier::RatioIndex::DIV4);
+    clockDivB.init();
+    clockDivB.channels[0].setRatio(ClockDividerMultiplier::RatioIndex::DIV3);
+    clockDivB.channels[1].setRatio(ClockDividerMultiplier::RatioIndex::DIV5);
+    clockDivB.channels[2].setRatio(ClockDividerMultiplier::RatioIndex::DIV7);
+
+    lfo.init(PWM_BIT);
+    lfo.setWaveform(SyncLFO::Waveform::TRIANGLE);
+    lfo.setRatio(SyncLFO::RatioIndex::DIV64);
+
+    initPWM(OUT_CV, PWM_RESO);
+    // initPWMIntr(PWM_INTR_PIN, interruptPWM, &interruptSliceNum, SAMPLE_FREQ, INTR_PWM_RESO, CPU_CLOCK);
+
+    pinMode(CLOCK, INPUT);
+    pinMode(RESET, INPUT);
+    gpio_set_irq_enabled(CLOCK, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(RESET, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(DATA, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_callback(edgeCallback);
+    irq_set_enabled(IO_IRQ_BANK0, true);
+}
+
+void loop()
+{
+    int8_t encValue = enc.getDirection();
+
+    process();
+
+    rgbLedControl.process();
+    tight_loop_contents();
+}
+
+void setup1()
+{
+}
+
+void loop1()
+{
+    int8_t encValue = enc.getValue();
+    uint8_t btnA = buttons[0].getState();
+    uint8_t btnB = buttons[1].getState();
+    uint8_t btnRE = buttons[2].getState();
+    uint8_t btnMode = buttons[3].getState();
+    // ButtonCondition用にまとめる
+    uint16_t buttonStates = (btnMode << 12) + (btnA << 8) + (btnB << 4) + btnRE;
+
+    operation(buttonStates, encValue);
+
+    rgbLedControl.update();
+    tight_loop_contents();
+    sleep_ms(10);
+}
---
+++ app/common/ClockDividerMultiplier.hpp
@@ -0,0 +1,442 @@
+/*!
+ * ClockDividerMultiplier class
+ * Copyright 2026 marksard
+ * This software is released under the MIT license.
+ * see https://opensource.org/licenses/MIT
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include "pico/stdlib.h"
+#include <vector>
+
+/// @brief Clock Divider / Multiplier
+/// @details
+/// 入力クロックを基準に、各チャンネルごとに
+/// Divide(分周)またはMultiply(逓倍)されたクロックを生成する。
+///
+/// Multiply動作はDDS(Direct Digital Synthesis)方式を使用する。
+///
+/// 特徴:
+/// - チャンネルごとに独立したDivide / Multiply設定
+/// - チャンネルごとに独立した倍率設定
+/// - Trigger出力または50% Gate出力
+/// - RESET入力対応
+/// - CLOCK入力停止時の自動タイムアウト停止
+///
+/// RESET入力を受けると、次回CLOCK立ち上がり時に
+/// 全チャンネルの位相をリセットする。
+class ClockDividerMultiplier
+{
+public:
+    static constexpr uint64_t PHASE_SCALE = 0x100000000ULL;
+
+    enum class PulseMode : uint8_t
+    {
+        TRIGGER,
+        GATE_50
+    };
+
+    enum class RatioIndex : uint8_t
+    {
+        DIV256,
+        DIV128,
+        DIV64,
+        DIV32,
+        DIV16,
+        DIV12,
+        DIV8,
+        DIV7,
+        DIV6,
+        DIV5,
+        DIV4,
+        DIV3,
+        DIV2,
+
+        CLK,
+
+        MUL2,
+        MUL3,
+        MUL4,
+        MUL5,
+        MUL6,
+        MUL7,
+        MUL8,
+        MUL12,
+        MUL16
+    };
+
+    struct RatioInfo
+    {
+        bool multiply;
+        uint16_t factor;
+    };
+
+    static constexpr RatioInfo RATIO_TABLE[] =
+        {
+            {false, 256},
+            {false, 128},
+            {false, 64},
+            {false, 32},
+            {false, 16},
+            {false, 12},
+            {false, 8},
+            {false, 7},
+            {false, 6},
+            {false, 5},
+            {false, 4},
+            {false, 3},
+            {false, 2},
+
+            {false, 1},
+
+            {true, 2},
+            {true, 3},
+            {true, 4},
+            {true, 5},
+            {true, 6},
+            {true, 7},
+            {true, 8},
+            {true, 12},
+            {true, 16}};
+
+    /// @brief チャンネル設定
+    struct Channel
+    {
+        PulseMode pulseMode = PulseMode::GATE_50;
+        uint8_t no = 0;
+        bool multiply = false;
+        uint16_t factor = 2;
+        bool outputState = false;
+        uint32_t lastUpdateUs = 0;
+        uint32_t pulseOffTimeUs = 0;
+        uint32_t phase = 0;        // multiply用
+        uint8_t divideCounter = 0; // divide用
+
+        RatioIndex ratioIndex = RatioIndex::CLK;
+        RatioIndex ratioIndexLower = RatioIndex::DIV64;
+        RatioIndex ratioIndexUpper = RatioIndex::MUL16;
+
+        void setRatioLower(RatioIndex r)
+        {
+            ratioIndexLower = r;
+        }
+
+        void setRatioUpper(RatioIndex r)
+        {
+            ratioIndexUpper = r;
+        }
+
+        void togglePulseMode()
+        {
+            pulseMode = pulseMode == PulseMode::TRIGGER ? PulseMode::GATE_50 : PulseMode::TRIGGER;
+        }
+
+        void addPulseMode(int8_t delta)
+        {
+            pulseMode = (PulseMode)constrain((int8_t)pulseMode + delta, (int8_t)PulseMode::TRIGGER, (int8_t)PulseMode::GATE_50);
+        }
+
+        void addRatio(int8_t delta)
+        {
+            RatioIndex r = (RatioIndex)constrain((int8_t)ratioIndex + delta, (int8_t)ratioIndexLower, (int8_t)ratioIndexUpper);
+            setRatio(r);
+        }
+
+        void setRatio(RatioIndex r)
+        {
+            ratioIndex = r;
+            multiply = RATIO_TABLE[(int8_t)r].multiply;
+            factor = RATIO_TABLE[(int8_t)r].factor;
+        }
+    };
+
+    uint8_t numChannels;
+    std::vector<Channel> channels;
+
+    ClockDividerMultiplier(uint8_t numChannels)
+        : numChannels(numChannels),
+          channels(numChannels)
+    {
+    }
+
+    void init()
+    {
+        uint32_t now = time_us_32();
+        lastClockUs = now;
+        int8_t no = 0;
+        for (auto &ch : channels)
+        {
+            ch.phase = 0;
+            ch.lastUpdateUs = now;
+            ch.outputState = false;
+            ch.pulseOffTimeUs = 0;
+            ch.no = no;
+            no++;
+        }
+    }
+
+    void allSetPulseMode(PulseMode value)
+    {
+        for (auto &ch : channels)
+        {
+            ch.pulseMode = value;
+        }
+    }
+
+    void allAddPulseMode(int8_t delta)
+    {
+        for (auto &ch : channels)
+        {
+            ch.addPulseMode(delta);
+        }
+    }
+
+    /// @brief Triggerモード時のパルス幅設定
+    /// @param us パルス幅[us]
+    void setTriggerWidthUs(uint32_t us)
+    {
+        triggerWidthUs = us;
+    }
+
+    /// @brief RESET入力立ち上がり通知
+    /// @details
+    /// 実際のリセットは次回CLOCK入力時に行われる。
+    void onResetRise(bool force = false)
+    {
+        resetPending = true;
+        if (force == true)
+        {
+            clockPeriodUs = 0;
+            lastClockUs = 0;
+        }
+    }
+
+    /// @brief RESET処理
+    /// @details
+    /// 実際のリセット
+    /// divide側はリセット時発火したいのでch.factorを入れてonClockRiseの中で強制発火させる
+    void doReset(uint32_t now)
+    {
+        for (auto &ch : channels)
+        {
+            if (ch.multiply)
+            {
+                ch.phase = 0;
+                ch.lastUpdateUs = now;
+            }
+            else
+            {
+                ch.divideCounter = ch.factor;
+            }
+
+            if (ch.outputState)
+            {
+                ch.outputState = false;
+                onOutputLow(ch.no);
+            }
+        }
+
+        resetPending = false;
+    }
+
+    /// @brief CLOCK入力立ち上がり通知
+    /// @details
+    /// クロック周期測定とDivide/Multiplyイベント生成を行う。
+    void onClockRise()
+    {
+        uint32_t now = time_us_32();
+
+        clockRunning = true;
+
+        if (lastClockUs != 0)
+        {
+            uint32_t period = now - lastClockUs;
+
+            if (period > 100)
+            {
+                clockPeriodUs = period;
+            }
+        }
+
+        lastClockUs = now;
+
+        if (resetPending)
+        {
+            doReset(now);
+        }
+
+        for (auto &ch : channels)
+        {
+            if (ch.multiply)
+            {
+                uint32_t intervalUs =
+                    clockPeriodUs / ch.factor;
+
+                fireEvent(ch.no, now, intervalUs);
+
+                // Hard Sync
+                ch.phase = 0;
+                ch.lastUpdateUs = now;
+            }
+            else
+            {
+                if (ch.factor == 1)
+                {
+                    fireEvent(
+                        ch.no,
+                        now,
+                        clockPeriodUs);
+                }
+                else
+                {
+                    ch.divideCounter++;
+
+                    if (ch.divideCounter >= ch.factor)
+                    {
+                        ch.divideCounter = 0;
+
+                        fireEvent(
+                            ch.no,
+                            now,
+                            clockPeriodUs * ch.factor);
+                    }
+                }
+            }
+        }
+    }
+
+    /// @brief メイン更新処理
+    /// @details
+    /// メインループから周期的に呼び出す。
+    ///
+    /// 以下を処理する:
+    /// - CLOCKタイムアウト監視
+    /// - パルスOFF処理
+    /// - Multiply用DDS更新
+    void update()
+    {
+        uint32_t now = time_us_32();
+
+        // Clock timeout
+        if (clockRunning)
+        {
+            uint32_t timeoutUs =
+                clockPeriodUs * 2;
+
+            if ((uint32_t)(now - lastClockUs) > timeoutUs)
+            {
+                clockRunning = false;
+
+                for (auto &ch : channels)
+                {
+                    ch.phase = 0;
+                    ch.lastUpdateUs = now;
+
+                    if (ch.outputState)
+                    {
+                        ch.outputState = false;
+                        onOutputLow(ch.no);
+                    }
+                }
+            }
+        }
+
+        for (auto &ch : channels)
+        {
+            // Pulse OFF
+            if (ch.outputState)
+            {
+                if ((int32_t)(now - ch.pulseOffTimeUs) >= 0)
+                {
+                    ch.outputState = false;
+                    onOutputLow(ch.no);
+                }
+            }
+
+            // Multiply DDS
+            if (!ch.multiply)
+                continue;
+
+            if (!clockRunning)
+                continue;
+
+            uint32_t dt = now - ch.lastUpdateUs;
+            ch.lastUpdateUs = now;
+            uint64_t increment =
+                (PHASE_SCALE * (uint64_t)ch.factor * (uint64_t)dt) / clockPeriodUs;
+            uint64_t sum = (uint64_t)ch.phase + increment;
+            uint32_t overflowCount = sum >> 32;
+            ch.phase = (uint32_t)sum;
+            uint32_t intervalUs = clockPeriodUs / ch.factor;
+
+            for (uint32_t i = 0; i < overflowCount; i++)
+            {
+                fireEvent(ch.no, now, intervalUs);
+            }
+        }
+    }
+
+protected:
+    /// @brief 出力をHIGHにする
+    /// @param ch チャンネル番号
+    /// @details
+    /// 継承先でgpio_put()などを実装する。
+    virtual void onOutputHigh(uint8_t ch)
+    {
+    }
+
+    /// @brief 出力をLOWにする
+    /// @param ch チャンネル番号
+    /// @details
+    /// 継承先でgpio_put()などを実装する。
+    virtual void onOutputLow(uint8_t ch)
+    {
+    }
+
+private:
+    uint32_t triggerWidthUs = 5000;
+    uint32_t clockPeriodUs = 500000;
+    uint32_t lastClockUs = 0;
+
+    bool resetPending = false;
+    bool clockRunning = false;
+
+    /// @brief 出力イベント発生
+    /// @param ch チャンネル番号
+    /// @param now 現在時刻[us]
+    /// @param intervalUs 出力周期[us]
+    /// @details
+    /// PulseModeに応じたパルス生成を行う。
+    void fireEvent(
+        uint8_t no,
+        uint32_t now,
+        uint32_t intervalUs)
+    {
+        auto &ch = channels[no];
+
+        switch (ch.pulseMode)
+        {
+        case PulseMode::TRIGGER:
+            ch.pulseOffTimeUs =
+                now + triggerWidthUs;
+            if (!ch.outputState)
+            {
+                ch.outputState = true;
+                onOutputHigh(no);
+            }
+            break;
+
+        case PulseMode::GATE_50:
+            ch.pulseOffTimeUs =
+                now + (intervalUs >> 1);
+            if (!ch.outputState)
+            {
+                ch.outputState = true;
+                onOutputHigh(no);
+            }
+            break;
+        }
+    }
+};
